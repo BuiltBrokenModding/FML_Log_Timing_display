24517c2ea16cb57496520228d8e2adac9ba12daa
Robert Seifert <rseifert.phone@gmail.com>
2017-09-03 18:45:08 -0400

    Added file options, Added display options, include more test data,

--- src/main/java/com/builtbroken/mc/logp/gui/Controller.java
+++ src/main/java/com/builtbroken/mc/logp/gui/Controller.java
@@ -36,7 +36,9 @@ public class Controller implements Initializable
     @Override
     public void initialize(URL location, ResourceBundle resources)
     {
-        //nothing to init
+        //Limit file types
+        FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("TXT files (*.txt)", "*.txt", "*.log");
+        fileChooser.getExtensionFilters().add(extFilter);
     }
 
     @FXML
@@ -46,82 +48,101 @@ public class Controller implements Initializable
         if (file != null)
         {
             parser.loadDataFromFile(file);
-            mainGUI.pieChart.setData(parser.buildPieChartData());
-
-            mainGUI.chartList.getItems().clear();
-
-            for (PieChart.Data data : mainGUI.pieChart.getData())
-            {
-                mainGUI.chartList.getItems().add(data.getName()); //TODO create custom factory to display with color
-            }
-
-            StringBuilder chartTextData = new StringBuilder();
-            chartTextData.append("Load data information\n");
-            chartTextData.append("--------------------------------------\n");
-            chartTextData.append("Entries: " + parser.data.values().size() + "\n");
-
-            long actualLoadTime = 0;
-            long excludedLoadIime = 0;
-            long totalTime = 0;
-
-            List<ModData> actualData = new ArrayList();
-            List<ModData> excludedData = new ArrayList();
-            for (ModData data : parser.data.values())
-            {
-                if (!parser.excludeFromDisplay(data.modName))
-                {
-                    actualData.add(data);
-                    actualLoadTime += data.getTime();
-                }
-                else
-                {
-                    excludedData.add(data);
-                    excludedLoadIime += data.getTime();
-                }
-            }
-
-            totalTime = actualLoadTime + excludedLoadIime;
-
-            //Sort values
-            Collections.sort(actualData, new ModDataTimeSorter());
-            Collections.sort(excludedData, new ModDataTimeSorter());
-
-            //Output totals for time
-            chartTextData.append("Total Time: " + totalTime + "ms \n");
-            chartTextData.append("Excluded Time: " + excludedLoadIime + "ms \n");
-            chartTextData.append("Actual Time: " + actualLoadTime + "ms \n");
-
-            //Output excluded entry data
-            chartTextData.append("--------------------------------------\n");
-            chartTextData.append("Excluded: " + excludedData.size() + "\n");
-            for (ModData data : excludedData)
-            {
-                float percentExcluded = (float) data.getTime() / (float) excludedLoadIime;
-                float percentTotal = (float) data.getTime() / (float) totalTime;
-                chartTextData.append(data.modName + ": " + formatTime(data.getTime()) + "ms \n");
-                chartTextData.append("\t Time percent: " + formatPercent(percentExcluded) + " \n");
-                chartTextData.append("\t Total time percent: " + formatPercent(percentTotal) + " \n");
-            }
-
-            //Output actual entry data
-            chartTextData.append("--------------------------------------\n");
-            chartTextData.append("Actual: " + excludedData.size() + "\n");
-            for (ModData data : actualData)
-            {
-                float percentExcluded = (float) data.getTime() / (float) actualLoadTime;
-                float percentTotal = (float) data.getTime() / (float) totalTime;
-                chartTextData.append(data.modName + ": " + formatTime(data.getTime()) + "ms \n");
-                chartTextData.append("\t Time percent: " + formatPercent(percentExcluded) + " \n");
-                chartTextData.append("\t Total time percent: " + formatPercent(percentTotal) + " \n");
-            }
-
-            //Build and add to display
-            mainGUI.chartDataTextArea.setText(chartTextData.toString());
+            loadPieChartDisplay();
+            loadModDataList();
+            createTextOutput();
         }
         else
         {
             //TODO display error?
         }
+    }
+
+    @FXML
+    public void reloadChartDisplay(ActionEvent actionEvent)
+    {
+
+    }
+
+    protected void loadPieChartDisplay()
+    {
+        mainGUI.pieChart.setData(parser.buildPieChartData());
+    }
+
+    protected void loadModDataList()
+    {
+        mainGUI.chartList.getItems().clear();
+
+        for (PieChart.Data data : mainGUI.pieChart.getData())
+        {
+            mainGUI.chartList.getItems().add(data.getName()); //TODO create custom factory to display with color
+        }
+    }
+
+    protected void createTextOutput()
+    {
+        StringBuilder chartTextData = new StringBuilder();
+        chartTextData.append("Load data information\n");
+        chartTextData.append("--------------------------------------\n");
+        chartTextData.append("Entries: " + parser.data.values().size() + "\n");
+
+        long actualLoadTime = 0;
+        long excludedLoadIime = 0;
+        long totalTime = 0;
+
+        List<ModData> actualData = new ArrayList();
+        List<ModData> excludedData = new ArrayList();
+        for (ModData data : parser.data.values())
+        {
+            if (!parser.excludeFromDisplay(data.modName))
+            {
+                actualData.add(data);
+                actualLoadTime += data.getTime();
+            }
+            else
+            {
+                excludedData.add(data);
+                excludedLoadIime += data.getTime();
+            }
+        }
+
+        totalTime = actualLoadTime + excludedLoadIime;
+
+        //Sort values
+        Collections.sort(actualData, new ModDataTimeSorter());
+        Collections.sort(excludedData, new ModDataTimeSorter());
+
+        //Output totals for time
+        chartTextData.append("Total Time: " + totalTime + "ms \n");
+        chartTextData.append("Excluded Time: " + excludedLoadIime + "ms \n");
+        chartTextData.append("Actual Time: " + actualLoadTime + "ms \n");
+
+        //Output excluded entry data
+        chartTextData.append("--------------------------------------\n");
+        chartTextData.append("Excluded: " + excludedData.size() + "\n");
+        for (ModData data : excludedData)
+        {
+            float percentExcluded = (float) data.getTime() / (float) excludedLoadIime;
+            float percentTotal = (float) data.getTime() / (float) totalTime;
+            chartTextData.append(data.modName + ": " + formatTime(data.getTime()) + "ms \n");
+            chartTextData.append("\t Time percent: " + formatPercent(percentExcluded) + " \n");
+            chartTextData.append("\t Total time percent: " + formatPercent(percentTotal) + " \n");
+        }
+
+        //Output actual entry data
+        chartTextData.append("--------------------------------------\n");
+        chartTextData.append("Actual: " + excludedData.size() + "\n");
+        for (ModData data : actualData)
+        {
+            float percentExcluded = (float) data.getTime() / (float) actualLoadTime;
+            float percentTotal = (float) data.getTime() / (float) totalTime;
+            chartTextData.append(data.modName + ": " + formatTime(data.getTime()) + "ms \n");
+            chartTextData.append("\t Time percent: " + formatPercent(percentExcluded) + " \n");
+            chartTextData.append("\t Total time percent: " + formatPercent(percentTotal) + " \n");
+        }
+
+        //Build and add to display
+        mainGUI.chartDataTextArea.setText(chartTextData.toString());
     }
 
     protected String formatTime(long time)
